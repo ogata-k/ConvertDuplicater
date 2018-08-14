a6623386bab09afa00abcc63f0beeca9882eb49e
ogata-k <ogtkzk@yahoo.co.jp>
2018-08-15 08:20:29 +0900

    make dummy-Image func to debug

--- src/Image.hs
+++ src/Image.hs
@@ -14,6 +14,15 @@ import Data.Time.Clock (getCurrentTime)
 import Data.Time.Format.ISO8601 (iso8601Show)
 import Data.Time.LocalTime (getCurrentTimeZone, utcToLocalTime)
 -- import qualified Codec.Picture.Types as M
+-- dummy Image
+makeDummyImg :: DynamicImage
+makeDummyImg = ImageRGB8 (generateImage originalFnc 2400 1200)
+    where
+        originalFnc :: Int -> Int -> PixelRGB8
+        originalFnc x y = 
+                let (q, r) = x `quotRem` max 10 y
+                    s      = fromIntegral . min 0xff
+                in PixelRGB8 (s q) (s r) (s (q + r + 30))
 
 -- 使用可能な色の指定方法（グレー、R系統、G系統、B系統）
 -- 使用可能な拡張子をpngだけにしてタイムスタンプ.pngを吐き出して保存するようにする
