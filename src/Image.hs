319e666ca2f6c8eb0f548a8c6057fd3ab192c9c5
ogata-k <ogtkzk@yahoo.co.jp>
2018-08-15 09:52:18 +0900

    make color index to convert easily.

--- src/Image.hs
+++ src/Image.hs
@@ -8,6 +8,7 @@ import Codec.Picture
 import Control.Monad
 import Control.Monad.ST
 import System.Directory (createDirectoryIfMissing)
+import Data.Word (Word8)
 import Data.Char (isDigit)
 import Data.Time.Clock (getCurrentTime)
 import Data.Time.Format.ISO8601 (iso8601Show)
@@ -24,9 +25,16 @@ makeDummyImg = ImageRGB8 (generateImage originalFnc 2400 1200)
                 in PixelRGB8 (s q) (s r) (s (q + r + 30))
 
 -- 使用可能な色の指定方法（グレー、R系統、G系統、B系統）
-getColorMagni :: Int -> Int -> Int
-getColorMagni minI maxI = undefined
-
+getColorMagni :: Int -> Int -> Word8  -- 色の指定範囲から0を起点とした255への範囲を作る倍率を作成
+getColorMagni minI maxI = fromIntegral $ floor (255.0 / fromIntegral (abs (maxI - minI)))  
+toPixel8 :: Int -> Word8 -> Int -> Pixel8  -- 0が起点ではないのでminIだけ平行移動させて正規化している
+toPixel8 minI magni target = (fromIntegral (target - minI)) * magni
+toGray = toPixel8
+toRed minI magni target = let r = toPixel8 minI magni target in PixelRGB8 r 0 0
+toGreen minI magni target = let g = toPixel8 minI magni target in PixelRGB8 0 g 0
+toBlue minI magni target = let b = toPixel8 minI magni target in PixelRGB8 0 0 b
+getColorIndex :: Pixel a => (Int -> a) -> Int -> Int -> [a] -- 使用可能な色のインデックス
+getColorIndex cnv minI maxI = map cnv [minI .. maxI]
 -- 使用可能な拡張子をpngだけにしてタイムスタンプ.pngを吐き出して保存するようにする
 getTimePngFilePath :: IO FilePath
 getTimePngFilePath = (++ ".png") . filter isDigit . iso8601Show <$> (utcToLocalTime <$> getCurrentTimeZone <*> getCurrentTime) 
@@ -37,5 +45,5 @@ savePngImageWithTStmp img = do
                                 savePngImage resPath img 
                                 return resPath
 
--- 画像への変換 注:使用可能な色数を指定させて変換できるようにすること
+-- 画像への変換
 
